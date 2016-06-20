8df36a1d74cb69e1911d15e311506aceae1c6612
Kevin Johnson <kevinlj246@yahoo.com>
2016-06-20 16:10:16 -0500

    Update feed.php

--- feed.php
+++ feed.php
@@ -27,28 +27,36 @@
 	$result_posts = mysqli_query($conn, "SELECT * FROM posts");
 	$num_of_rows = mysqli_num_rows($result_posts);
 
-	$result_comments = mysqli_query($conn, "SELECT * FROM comments");
-	$num_of_rows2 = mysqli_num_rows($result_comments);
-
-	$row_comment = mysqli_fetch_assoc($result_posts);
-	$PID = $row_comment['id'];
-
 	echo "<h2>My Feed</h2>";
 	if ($num_of_rows == 0) {
 		echo "<p>No new posts to show!</p>";
 	}
 
 	//show all posts 
-	for($i = 1; $i < $num_of_rows; $i++){
-		$row2 = mysqli_fetch_row($result_posts);
-		echo "$row2[2] said $row2[4] ($row2[5])";
-		echo "<form action='likes.php' method='POST'> <input type='hidden' name='PID' value='$row2[0]'> <input type='submit' value='Like'></form>";
+	for($i = 0; $i < $num_of_rows; $i++){
+		$row = mysqli_fetch_row($result_posts);
+		echo "$row[2] said $row[4] ($row[5])";
+		echo "<form action='likes.php' method='POST'> <input type='hidden' name='PID' value='$row[0]'> <input type='submit' value='Like'></form>";
 
-		$row3 = mysqli_fetch_row($result_comments);
-		echo "&emsp;$row3[3] commented \"$row3[1]\" on $row3[4]<br>";
-		echo "<form action='comments.php' method='POST'> <input type='hidden' name='UID' value='$row[id]'> <p><textarea name='comment'>Comment...</textarea></p> <input type='submit' value='Comment'></form>";
-
-		echo "<br>";
+		$result_comments = mysqli_query($conn, "SELECT * FROM comments WHERE postID='$row[0]'");
+		$num_of_rows2 = mysqli_num_rows($result_comments);
+		//show comments
+        if($num_of_rows2){
+        	for($j =0; $j < $num_of_rows2; $j++){
+            	$row2 = mysqli_fetch_row($result_comments);
+			  	echo "&emsp;$row2[3] commented \"$row2[1]\" on $row2[4]<br>";
+              	echo "<br>";
+            }
+        }else{
+           	echo "No comments to show.";
+            echo "<br>";
+       	}
+        echo "<form action='comments.php' method='POST'>";
+        echo "<textarea name='comment'>Comment...</textarea>";
+        echo "<input type='hidden' name='PID' value='$row[0]'>";
+        echo "<input type='hidden' name='username' value='$username'>";
+        echo "<input type='submit' value='Comment'></form>";
+        echo "<br>";
 	}
 	echo '<a href="logout.php">Click here</a> to log out.';
 ?>
