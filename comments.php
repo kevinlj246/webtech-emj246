f252997b479e4a94a253118d9fde94cf718ef157
Kevin Johnson <kevinlj246@yahoo.com>
2016-06-20 16:09:46 -0500

    Update comments.php

--- comments.php
+++ comments.php
@@ -6,28 +6,20 @@
 	include('functions.php');
 
 		//Get data from the form
-	$comment = $_POST['comment'];
-	$UID = $_POST['UID'];
-	$PID = $_POST['PID'];
+	$comment = sanitizeString($_POST['comment']);
+    $PID = sanitizeString($_POST['PID']);
+    $username = sanitizeString($_POST['username']);
 
 	//connect to DB
 	$conn = connect_db();
-	$result = mysqli_query($conn, "SELECT * FROM users WHERE id = '$UID'");
+	$result = mysqli_query($conn, "SELECT * FROM users WHERE username = '$username'");
 	$row = mysqli_fetch_assoc($result);
 
-	$result2 = mysqli_query($conn, "SELECT * FROM posts WHERE id = '$PID'");
-	$row2 = mysqli_fetch_assoc($result2);
-
-
-
 	//Fetch User information	
-	$name = $row["name"];
-	$postID = $row2["postID"];
-	sanitizeString($comment);
+	$name = sanitizeString($row["name"]);
+	$UID = sanitizeString($row["id"]);
 
-
-
-	$result_insert = mysqli_query($conn, "INSERT INTO comments(postID, comment, userID, nameOfUser) VALUES ('$postID', '$comment', $UID, '$name')");
+	$result_insert = mysqli_query($conn, "INSERT INTO comments(postID, comment, userID, nameOfUser) VALUES ('$PID', '$comment', $UID, '$name')");
 
 	//check if insert was okay
 	if($result_insert){
@@ -38,6 +30,5 @@
 	}else{
 		//throw an error	
 		echo "Oops! Something went wrong! Please try again!";
-
 	}
 ?>
